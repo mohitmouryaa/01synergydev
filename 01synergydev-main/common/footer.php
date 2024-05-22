f66a18b0ca2cfed981ee37b3a5d336dff91d85b7
Naveen Singh <101163510+naveensynergy7@users.noreply.github.com>
2024-05-22 20:41:48 +0530

    links fix

--- 01synergydev-main/common/footer.php
+++ 01synergydev-main/common/footer.php
@@ -1,86 +1,86 @@
 <footer id="footer" class="xl-top-5 xs-top-3">
-        <div class="bs-xl-22 bs-xl-os-1">
-          <div class="bs-xl-10 bs-xl-os-9 bs-xs-22 bs-xs-os-0">
-            <h3 class="contact-link bs-120 upper">
-              <a href="../contact" class="mask-link" data-id="LET’S TALK" aria-label="LET’S TALK"><span>LET’S TALK</span><i class="dot work-dot"><svg viewBox="0 0 10 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <div class="bs-xl-22 bs-xl-os-1">
+    <div class="bs-xl-10 bs-xl-os-9 bs-xs-22 bs-xs-os-0">
+      <!-- <h3 class="contact-link bs-120 upper">
+        <a href="../contact" class="mask-link" data-id="LET’S TALK" aria-label="LET’S TALK"><span>LET’S TALK</span><i class="dot work-dot"><svg viewBox="0 0 10 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd" clip-rule="evenodd" d="M10 8.12543L5 11L-2.25534e-07 8.12543L2.3356e-07 2.87401L5 2.18537e-07L10 2.87401L10 8.12543Z" fill="#C49E76"></path>
+            </svg></i></a>
+      </h3> -->
+    </div>
+    <div class="line-anim">
+      <svg width="11" height="9" xmlns="http://www.w3.org/2000/svg">
+        <path d="m10.407 4.5-2.555 4H3.148l-2.555-4 2.555-4h4.704l2.555 4Z" fill="#1D2145" stroke="#EEEEF2" fill-rule="evenodd"></path>
+      </svg>
+      <div></div>
+      <svg width="11" height="9" xmlns="http://www.w3.org/2000/svg">
+        <path d="m10.407 4.5-2.555 4H3.148l-2.555-4 2.555-4h4.704l2.555 4Z" fill="#1D2145" stroke="#EEEEF2" fill-rule="evenodd"></path>
+      </svg>
+    </div>
+
+    <div class="bs-grid xs-wrap">
+      <div class="bs-xl-8 bs-xs-22">
+        <h3 class="bs-120 upper">
+          <a href="/"><span>
+              <img src="<?= $baseUrl . $settingData['websiteSettings']['footerContent']['footerLogo'] ?>" style="width: 200px;" />
+            </span></a>
+        </h3>
+        <address class="bs-xl-6 bs-xs-22 xl-top-1">
+          <?php
+          $contactAddress = $settingData['websiteSettings']['footerContent']['contactAddress'];
+          $decodedAddress = html_entity_decode($contactAddress, ENT_QUOTES, 'UTF-8');
+          ?>
+          <div>
+            <?php echo $decodedAddress; ?>
+          </div>
+        </address>
+      </div>
+
+      <div class="bs-xl-13 bs-xl-os-1 bs-xs-22 bs-xs-os-0 xs-top-1">
+        <div class="bs-grid grid-between grid-bottom xs-wrap">
+          <div class="bs-xl-4 bs-xs-14">
+            <ul class="service-links">
+              <?php $services = @$homepageData['whatWeDo']['list'] ? @$homepageData['whatWeDo']['list'] : $servicesData['list']; ?>
+              <?php foreach ($services as $key => $item) {  ?>
+                <li>
+                  <a class="text-link" href="<?php echo $item['url']; ?>"><span><?php echo $item['title']; ?></span></a>
+                </li>
+              <?php } ?>
+            </ul>
+          </div>
+          <div class="bs-xl-9 right bs-xs-12">
+            <h3 class="bs-120 upper">
+              <a href="/services" class="mask-link" data-id="Services" aria-label="Services">
+                <span>Services</span><i class="dot work-dot">
+                  <svg viewBox="0 0 10 11" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd" clip-rule="evenodd" d="M10 8.12543L5 11L-2.25534e-07 8.12543L2.3356e-07 2.87401L5 2.18537e-07L10 2.87401L10 8.12543Z" fill="#C49E76"></path>
-                  </svg></i></a>
+                  </svg>
+                </i>
+              </a>
             </h3>
           </div>
-          <div class="line-anim">
-            <svg width="11" height="9" xmlns="http://www.w3.org/2000/svg">
-              <path d="m10.407 4.5-2.555 4H3.148l-2.555-4 2.555-4h4.704l2.555 4Z" fill="#1D2145" stroke="#EEEEF2" fill-rule="evenodd"></path>
-            </svg>
-            <div></div>
-            <svg width="11" height="9" xmlns="http://www.w3.org/2000/svg">
-              <path d="m10.407 4.5-2.555 4H3.148l-2.555-4 2.555-4h4.704l2.555 4Z" fill="#1D2145" stroke="#EEEEF2" fill-rule="evenodd"></path>
-            </svg>
-          </div>
+        </div>
+        <div class="line-anim">
+          <svg width="11" height="9" xmlns="http://www.w3.org/2000/svg">
+            <path d="m10.407 4.5-2.555 4H3.148l-2.555-4 2.555-4h4.704l2.555 4Z" fill="#1D2145" stroke="#EEEEF2" fill-rule="evenodd"></path>
+          </svg>
+          <div></div>
+          <svg width="11" height="9" xmlns="http://www.w3.org/2000/svg">
+            <path d="m10.407 4.5-2.555 4H3.148l-2.555-4 2.555-4h4.704l2.555 4Z" fill="#1D2145" stroke="#EEEEF2" fill-rule="evenodd"></path>
+          </svg>
+        </div>
+        <h3 class="bs-120 upper">
+          <a href="/our-work" class="mask-link" data-id="work" aria-label="Work"><span>Work</span><i class="dot work-dot">
+              <svg viewBox="0 0 10 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M10 8.12543L5 11L-2.25534e-07 8.12543L2.3356e-07 2.87401L5 2.18537e-07L10 2.87401L10 8.12543Z" fill="#C49E76"></path>
+              </svg></i>
+          </a>
+        </h3>
+      </div>
+    </div>
 
-          <div class="bs-grid xs-wrap">
-            <div class="bs-xl-8 bs-xs-22">
-              <h3 class="bs-120 upper">
-                <a href="/"><span>
-                    <img src="<?= $baseUrl . $settingData['websiteSettings']['footerContent']['footerLogo'] ?>" style="width: 200px;" />
-                  </span></a>
-              </h3>
-              <address class="bs-xl-6 bs-xs-22 xl-top-1">
-                <?php
-                $contactAddress = $settingData['websiteSettings']['footerContent']['contactAddress'];
-                $decodedAddress = html_entity_decode($contactAddress, ENT_QUOTES, 'UTF-8');
-                ?>
-                <div>
-                  <?php echo $decodedAddress; ?>
-                </div>
-              </address>
-            </div>
-
-            <div class="bs-xl-13 bs-xl-os-1 bs-xs-22 bs-xs-os-0 xs-top-1">
-              <div class="bs-grid grid-between grid-bottom xs-wrap">
-                <div class="bs-xl-4 bs-xs-14">
-                  <ul class="service-links">
-                    <?php $services = @$homepageData['whatWeDo']['list'] ? @$homepageData['whatWeDo']['list'] : $servicesData['list'] ; ?>
-                    <?php foreach ( $services as $key => $item) {  ?>
-                      <li>
-                        <a class="text-link" href="<?php echo $item['url']; ?>"><span><?php echo $item['title']; ?></span></a>
-                      </li>
-                    <?php } ?>
-                  </ul>
-                </div>
-                <div class="bs-xl-9 right bs-xs-12">
-                  <h3 class="bs-120 upper">
-                    <a href="/services" class="mask-link" data-id="Services" aria-label="Services">
-                      <span>Services</span><i class="dot work-dot">
-                        <svg viewBox="0 0 10 11" fill="none" xmlns="http://www.w3.org/2000/svg">
-                          <path fill-rule="evenodd" clip-rule="evenodd" d="M10 8.12543L5 11L-2.25534e-07 8.12543L2.3356e-07 2.87401L5 2.18537e-07L10 2.87401L10 8.12543Z" fill="#C49E76"></path>
-                        </svg>
-                      </i>
-                    </a>
-                  </h3>
-                </div>
-              </div>
-              <div class="line-anim">
-                <svg width="11" height="9" xmlns="http://www.w3.org/2000/svg">
-                  <path d="m10.407 4.5-2.555 4H3.148l-2.555-4 2.555-4h4.704l2.555 4Z" fill="#1D2145" stroke="#EEEEF2" fill-rule="evenodd"></path>
-                </svg>
-                <div></div>
-                <svg width="11" height="9" xmlns="http://www.w3.org/2000/svg">
-                  <path d="m10.407 4.5-2.555 4H3.148l-2.555-4 2.555-4h4.704l2.555 4Z" fill="#1D2145" stroke="#EEEEF2" fill-rule="evenodd"></path>
-                </svg>
-              </div>
-              <h3 class="bs-120 upper">
-                <a href="/our-work" class="mask-link" data-id="work" aria-label="Work"><span>Work</span><i class="dot work-dot">
-                    <svg viewBox="0 0 10 11" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path fill-rule="evenodd" clip-rule="evenodd" d="M10 8.12543L5 11L-2.25534e-07 8.12543L2.3356e-07 2.87401L5 2.18537e-07L10 2.87401L10 8.12543Z" fill="#C49E76"></path>
-                    </svg></i>
-                </a>
-              </h3>
-            </div>
-          </div>
-
-          <div class="bs-grid footer-bottom grid-bottom xs-top-1 sm-wrap">
-            <div id="newsletter" class="bs-xl-8 bs-sm-22 sm-top-1">
-              <h2 class="bs-tech">Newsletter</h2>
+    <div class="bs-grid footer-bottom grid-bottom xs-top-1 sm-wrap">
+      <div id="newsletter" class="bs-xl-8 bs-sm-22 sm-top-1">
+        <!-- <h2 class="bs-tech">Newsletter</h2>
               <form action="">
                 <input type="email" placeholder="Your Email" name="mail" required="" />
                 <div class="email-underline"></div>
@@ -93,34 +93,23 @@
                     </svg>
                   </i>
                 </div>
-              </form>
-            </div>
-            <div class="bs-xl-13 bs-xl-os-1 bs-sm-22 bs-sm-os-0 sm-top-1">
-              <div class="bs-grid grid-between xs-wrap">
-                <div class="bs-xl-5 bs-sm-8 bs-xs-22 xs-center">
-                  <small class="bs-14"><?= $settingData['websiteSettings']['footerContent']['copyrightContent'] ?></small>
-                </div>
-                <div class="bs-xl-8 bs-sm-14 bs-xs-22 right xs-center xs-top-1">
-                  <ul class="bs-14">
-                    <li>
-                      <a href="https://www.linkedin.com/company/buzzworthystudio/" target="_blank" class="text-link" aria-label="Linkedin">Linkedin</a>
-                    </li>
-                    <li>
-                      <a href="https://www.instagram.com/buzzworthy.studio/" target="_blank" class="text-link" aria-label="Instagram">Instagram</a>
-                    </li>
-                    <li>
-                      <a href="https://twitter.com/getbuzzworthy" target="_blank" class="text-link" aria-label="Twitter">Twitter</a>
-                    </li>
-                    <li>
-                      <a href="https://www.behance.net/BuzzworthyStudio" target="_blank" class="text-link" aria-label="Behance">Behance</a>
-                    </li>
-                    <li>
-                      <a href="https://dribbble.com/01Synergy/" target="_blank" class="text-link" aria-label="Dribbble">Dribbble</a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
+              </form> -->
+      </div>
+      <div class="bs-xl-13 bs-xl-os-1 bs-sm-22 bs-sm-os-0 sm-top-1">
+        <div class="bs-grid grid-between xs-wrap">
+          <div class="bs-xl-5 bs-sm-8 bs-xs-22 xs-center">
+            <small class="bs-14"><?= $settingData['websiteSettings']['footerContent']['copyrightContent'] ?></small>
+          </div>
+          <div class="bs-xl-8 bs-sm-14 bs-xs-22 right xs-center xs-top-1">
+            <ul class="bs-14">
+              <a href="<?= @$settingData['websiteSettings']['footerContent']['twitterUrl'] ?>" target="_blank" class="text-link" aria-label="Twitter">Twitter</a> &nbsp;
+              <a href="<?= @$settingData['websiteSettings']['footerContent']['instagramUrl'] ?>" target="_blank" class="text-link" aria-label="Instagram">Instagram</a> &nbsp;
+              <a href="<?= @$settingData['websiteSettings']['footerContent']['facebookUrl'] ?>" target="_blank" class="text-link" aria-label="Facebook">Facebook</a>
+
+            </ul>
           </div>
         </div>
-      </footer>
+      </div>
+    </div>
+  </div>
+</footer>
--- 01synergydev-main/common/menu.php
+++ 01synergydev-main/common/menu.php
@@ -1,44 +1,44 @@
 <div id="menu-slide">
-    <div>
-      <nav id="links">
-        <div>
+  <div>
+    <nav id="links">
+      <div>
         <a class="nav-link mask-link" data-id="home" href="/" aria-label="Home"><span>Home</span>
-            <i class="dot work-dot">
-              <svg viewBox="0 0 10 11" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd" clip-rule="evenodd" d="M10 8.12543L5 11L-2.25534e-07 8.12543L2.3356e-07 2.87401L5 2.18537e-07L10 2.87401L10 8.12543Z"></path>
-              </svg>
-            </i>
-          </a>
+          <i class="dot work-dot">
+            <svg viewBox="0 0 10 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd" clip-rule="evenodd" d="M10 8.12543L5 11L-2.25534e-07 8.12543L2.3356e-07 2.87401L5 2.18537e-07L10 2.87401L10 8.12543Z"></path>
+            </svg>
+          </i>
+        </a>
         <a class="nav-link mask-link" data-id="work" href="/our-work" aria-label="Work"><span>Work</span>
-            <i class="dot work-dot">
-              <svg viewBox="0 0 10 11" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd" clip-rule="evenodd" d="M10 8.12543L5 11L-2.25534e-07 8.12543L2.3356e-07 2.87401L5 2.18537e-07L10 2.87401L10 8.12543Z"></path>
-              </svg>
-            </i>
-          </a>
-          <a class="nav-link mask-link" data-id="Services" href="/services" aria-label="Services"><span>Services</span>
-            <i class="dot work-dot"><svg viewBox="0 0 10 11" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd" clip-rule="evenodd" d="M10 8.12543L5 11L-2.25534e-07 8.12543L2.3356e-07 2.87401L5 2.18537e-07L10 2.87401L10 8.12543Z"></path>
-              </svg>
-            </i>
-          </a>
-        </div>
-      </nav>
-      <div class="social bs-12">
-
-          <a href="<?=@$settingData['footerContent']['twitterUrl']?>" target="_blank" class="text-link" aria-label="Twitter">Twitter</a>
-          <a href="<?=@$settingData['footerContent']['instagramUrl']?>" target="_blank" class="text-link" aria-label="Instagram">Instagram</a>
-          <a href="<?=@$settingData['footerContent']['facebookUrl']?>" target="_blank" class="text-link" aria-label="Facebook">Facebook</a>
-
+          <i class="dot work-dot">
+            <svg viewBox="0 0 10 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd" clip-rule="evenodd" d="M10 8.12543L5 11L-2.25534e-07 8.12543L2.3356e-07 2.87401L5 2.18537e-07L10 2.87401L10 8.12543Z"></path>
+            </svg>
+          </i>
+        </a>
+        <a class="nav-link mask-link" data-id="Services" href="/services" aria-label="Services"><span>Services</span>
+          <i class="dot work-dot"><svg viewBox="0 0 10 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd" clip-rule="evenodd" d="M10 8.12543L5 11L-2.25534e-07 8.12543L2.3356e-07 2.87401L5 2.18537e-07L10 2.87401L10 8.12543Z"></path>
+            </svg>
+          </i>
+        </a>
       </div>
+    </nav>
+    <div class="social bs-12">
+
+      <a href="<?= @$settingData['websiteSettings']['footerContent']['twitterUrl'] ?>" target="_blank" class="text-link" aria-label="Twitter">Twitter</a> &nbsp;
+      <a href="<?= @$settingData['websiteSettings']['footerContent']['instagramUrl'] ?>" target="_blank" class="text-link" aria-label="Instagram">Instagram</a> &nbsp;
+      <a href="<?= @$settingData['websiteSettings']['footerContent']['facebookUrl'] ?>" target="_blank" class="text-link" aria-label="Facebook">Facebook</a>
+
     </div>
   </div>
+</div>
 
-  <div class="page-to-page">
-    <svg viewBox="0 0 1420 880" fill="none" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none">
-      <path id="pageBgAnim2" d="M0 20C0 8.95429 8.86995 0 19.9156 0C68.0089 0 189.957 0 236.667 0C317.505 0 392.495 0 473.333 0C556.906 0 626.428 0 710 0C800.016 0 856.651 0 946.667 0C1067.34 0 1062.66 0 1183.33 0C1273.85 0 1361.73 0 1400 0C1411.05 0 1420 8.95431 1420 20V860C1420 871.046 1411.05 880 1400 880H20C8.95432 880 0 871.046 0 860V20Z" fill="#1D2145"></path>
-    </svg>
-    <svg viewBox="0 0 1420 880" fill="none" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none">
-      <path id="pageBgAnim" d="M0 20C0 8.95429 8.86995 0 19.9156 0C68.0089 0 189.957 0 236.667 0C317.505 0 392.495 0 473.333 0C556.906 0 626.428 0 710 0C800.016 0 856.651 0 946.667 0C1067.34 0 1062.66 0 1183.33 0C1273.85 0 1361.73 0 1400 0C1411.05 0 1420 8.95431 1420 20V860C1420 871.046 1411.05 880 1400 880H20C8.95432 880 0 871.046 0 860V20Z" fill="#1D2145"></path>
-    </svg>
-  </div>
+<div class="page-to-page">
+  <svg viewBox="0 0 1420 880" fill="none" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none">
+    <path id="pageBgAnim2" d="M0 20C0 8.95429 8.86995 0 19.9156 0C68.0089 0 189.957 0 236.667 0C317.505 0 392.495 0 473.333 0C556.906 0 626.428 0 710 0C800.016 0 856.651 0 946.667 0C1067.34 0 1062.66 0 1183.33 0C1273.85 0 1361.73 0 1400 0C1411.05 0 1420 8.95431 1420 20V860C1420 871.046 1411.05 880 1400 880H20C8.95432 880 0 871.046 0 860V20Z" fill="#1D2145"></path>
+  </svg>
+  <svg viewBox="0 0 1420 880" fill="none" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none">
+    <path id="pageBgAnim" d="M0 20C0 8.95429 8.86995 0 19.9156 0C68.0089 0 189.957 0 236.667 0C317.505 0 392.495 0 473.333 0C556.906 0 626.428 0 710 0C800.016 0 856.651 0 946.667 0C1067.34 0 1062.66 0 1183.33 0C1273.85 0 1361.73 0 1400 0C1411.05 0 1420 8.95431 1420 20V860C1420 871.046 1411.05 880 1400 880H20C8.95432 880 0 871.046 0 860V20Z" fill="#1D2145"></path>
+  </svg>
+</div>
